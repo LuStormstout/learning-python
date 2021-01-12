d44eefe449d501f4635d2a3c6dc393f3c28f624f
Jacklu <jacklu.net@gmail.com>
2021-01-12 16:47:11 +0800

    正则表达式函数练习

--- python-engineer/first-stage-of-python-basics/week7-regular/re_test2.py
+++ python-engineer/first-stage-of-python-basics/week7-regular/re_test2.py
@@ -4,8 +4,31 @@
 import re
 
 
+# re.findall(pattern, str, [, flags])
+#   查找字符串中所有（非重复）出现的正则表达式模式，并返回一个匹配列表
+#
+# re.search(pattern, str, flags=0)
+#   使用可选的标记搜索字符串中第一次出现的正则表达式模式，如果匹配成功，则返回匹配对象；如果失败，则返回 None
+#
+# re_search_obj.group(num)
+#   返回整个匹配对象，或者编号为 num 的特定子组
+#
+# re_search_obj.groups()
+#   返回一个包含所有匹配子组的元组（如果没有成功匹配，则返回一个空元组）
+#
+# re.split(pattern, str, max=0)
+#   根据正则表达式的模式分隔符，split 函数将字符串分隔为列表，然后返回成功匹配的列表，分隔最多操作 max 次（默认分隔所有匹配成功的位置）
+#
+# re.compile(pattern, flags=0)
+#   定义一个匹配规则的对象
+#
+# re.match(pattern, str, flags=0)
+#   尝试使用带有可选的标记的正则表达式的模式来匹配字符串。如果匹配成功，就返回匹配对象；如果失败就返回 None，返回的对象可以通过 group 来调用
+
+
 def check_url(url):
-    result = re.findall(r'[a-zA-Z]{4,5}://\w*\.*\w+\.\w+', url)
+    re_g = re.compile(r'[a-zA-Z]{4,5}://\w*\.*\w+\.\w+')
+    result = re_g.findall(url)
     if len(result) != 0:
         return True
     else:
@@ -13,7 +36,8 @@ def check_url(url):
 
 
 def get_email(data):
-    result = re.findall(r'.+@.+\.[a-zA-Z]+', data)
+    re_g = re.compile(r'.+@.+\.[a-zA-Z]+')
+    result = re_g.findall(data)
     return result
 
 
@@ -22,12 +46,14 @@ html = ('<div class="s-top-nav" style="display:none;">'
 
 
 def get_html_data(data):
-    result = re.findall(r'style="(.*?)"', data)
+    re_g = re.compile(r'style="(.*?)"')
+    result = re_g.findall(data)
     return result
 
 
 def get_all_html_data(data):
-    result = re.findall(r'="(.+?)"', data)
+    re_g = re.compile(r'="(.+?)"')
+    result = re_g.findall(data)
     return result
 
 
@@ -43,3 +69,18 @@ if __name__ == '__main__':
 
     all_html_data = get_all_html_data(html)
     print(all_html_data)
+
+    re_obj = re.compile(('<div class="(.*?)" style="(.*?)">'
+        '</div><div class="(.*?)"></div>'))
+    res = re_obj.search(html)
+    print(res.groups())
+
+    re_obj = re.compile(r'\s')
+    res = re_obj.split(html)
+    print(res)
+
+    re_obj = re.compile(r'<div class="(.*?)"')
+    res = re_obj.match(html)
+    print(res.span())
+    print(html[:22])
+
