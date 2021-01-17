37e5c02b4e755c3d2b81d7784a1415a7c4753443
Jacklu <jacklu.net@gmail.com>
2021-01-18 00:48:47 +0800

    抽奖功能开发

--- python-engineer/first-stage-of-python-basics/gift/user.py
+++ python-engineer/first-stage-of-python-basics/gift/user.py
@@ -1,8 +1,17 @@
 # coding:utf-8
 
+"""
+    1、抽奖函数 随机判断第一层（level1） 1：50%  2：30%  3：15%  4：5%
+    2、抽奖函数 随机判断第二层（level2） 1：80%  2：15%  3：5%
+    3、抽奖函数 获取到对应层级的真是奖品，并随机一个奖品，查看奖品 count 是否为 0，不为零则中奖同时提醒用户且奖品数量减 1，更新奖品到 user 表
+              中的 gifts 中
+              数量为 0，则未中奖
+"""
+
 import os
+import random
 from base import Base
-from common.error import NotUserError, RoleError, UserActiveError
+from common.error import NotUserError, RoleError, UserActiveError, CountError
 from common.utils import timestamp_to_string
 
 
@@ -10,10 +19,13 @@ class User(Base):
     def __init__(self, username, user_json, gift_json):
         self.username = username
 
+        self.user = None
         self.name = None
         self.role = None
         self.gifts = None
         self.create_time = None
+
+        self.gift_random = list(range(1, 101))
 
         super(User, self).__init__(user_json, gift_json)
         self.get_user()
@@ -32,6 +44,7 @@ class User(Base):
         if current_user.get('role') != 'normal':
             raise RoleError('permission denied by normal')
 
+        self.user = current_user
         self.name = current_user.get('username')
         self.role = current_user.get('role')
         self.gifts = current_user.get('gifts')
@@ -47,6 +60,63 @@ class User(Base):
                     gift_list.append(gift_info.get('name'))
         return gift_list
 
+    def choice_gist(self):
+        self.get_user()
+        first_level, second_level = None, None
+        level_one_count = random.choice(self.gift_random)
+
+        if 1 <= level_one_count <= 50:
+            first_level = 'level1'
+        elif 51 <= level_one_count <= 80:
+            first_level = 'level2'
+        elif 81 <= level_one_count < 95:
+            first_level = 'level3'
+        elif level_one_count >= 95:
+            first_level = 'level4'
+        else:
+            raise CountError('level_one_count should be 0-100')
+        gifts = self._Base__read_gifts()
+        level_one = gifts.get(first_level)
+
+        level_two_count = random.choice(self.gift_random)
+        if 1 <= level_two_count <= 80:
+            second_level = 'level1'
+        elif 81 <= level_two_count < 95:
+            second_level = 'level2'
+        elif 95 <= level_two_count < 100:
+            second_level = 'level3'
+        else:
+            raise CountError('level_two_count should be 0-100')
+        level_two = level_one.get(second_level)
+
+        if len(level_two) == 0:
+            print('可惜 你没有中奖')
+            return False
+        gift_names = []
+        for k, _ in level_two.items():
+            gift_names.append(k)
+
+        gift_name = random.choice(gift_names)
+        gift_info = level_two.get(gift_name)
+        if gift_info.get('count') <= 0:
+            print('可惜 你没有中奖')
+            return False
+
+        gift_info['count'] -= 1
+        level_two[gift_name] = gift_info
+        level_one[second_level] = level_two
+        gifts[first_level] = level_one
+        self._Base__save(gifts, self.gift_json)
+
+        self.user['gifts'].append(gift_name)
+        self.update()
+        print(f'恭喜你获得 {gift_name}')
+
+    def update(self):
+        users = self._Base__read_users()
+        users[self.username] = self.user
+        self._Base__save(users, self.user_json)
+
 
 if __name__ == '__main__':
     user_path = os.path.join(os.getcwd(), 'storage', 'user.json')
@@ -58,5 +128,7 @@ if __name__ == '__main__':
     # print(user.gifts)
     # print(user.role)
 
-    result = user.get_gifts()
-    print(result)
+    # result = user.get_gifts()
+    # print(result)
+
+    user.choice_gist()
