e572069a6f4d8b7ca6d01bcddcfffaadcd442820
Jacklu <jacklu.net@gmail.com>
2021-01-19 08:01:01 +0800

    删除不用的 sql

--- python-engineer/second-stage-python-database/select聚合函数和分组练习.sql
+++ python-engineer/second-stage-python-database/select聚合函数和分组练习.sql
@@ -1,7 +1,3 @@
-USE demo;
-
-SELECT * FROM t_emp;
-
 SELECT AVG(sal+IFNULL(comm,0)) FROM t_emp;
 
 SELECT SUM(sal) FROM t_emp WHERE deptno IN(10,20);
@@ -21,7 +17,7 @@ SELECT COUNT(*) FROM t_emp WHERE deptno IN(10,20) AND sal >= 2000 AND DATEDIFF(N
 -- ROUND() 四舍五入取整数
 SELECT deptno, ROUND(AVG(sal)) AS deptno_avg FROM t_emp GROUP BY deptno ORDER BY deptno;
 
-SELECT deptno, job, COUNT(*) AS job_count, AVG(sal) AS sal_avg FROM t_emp GROUP BY deptno, job ORDER BY deptno ASC;
+SELECT deptno, job, COUNT(*) AS job_count, AVG(sal) AS sal_avg FROM t_emp GROUP BY deptno, job ORDER BY deptno;
 
 -- WITH ROLLUP 对聚合函数的执行结果再次执行同样聚合函数的统计
 SELECT deptno, COUNT(*), AVG(sal), MAX(sal), MIN(sal) FROM t_emp GROUP BY deptno WITH ROLLUP;
